9c3c2d173f335fc64f42b204f05795f7f6161f77
Richard Bramley <rabramley@gmail.com>
2020-08-29 16:15:18 +0100

    Top wall

--- pong.py
+++ pong.py
@@ -3,30 +3,50 @@
 # Import and initialize the pygame library
 import pygame
 
+SCREEN_WIDTH = 500
+SCREEN_HEIGHT = 500
+
+WALL_WIDTH = 10
+
+WALL_COLOR = (255, 255, 255)
+TEXT_COLOR = (255, 255, 255)
+BACKGROUND_COLOR = (0, 0, 0)
+
+walls = pygame.sprite.Group()
+
+class Wall(pygame.sprite.Sprite):
+
+    def __init__(self, width, height, left, top):
+       pygame.sprite.Sprite.__init__(self, walls)
+
+       self.image = pygame.Surface([width, height])
+       self.image.fill(WALL_COLOR)
+
+       self.rect = self.image.get_rect()
+       self.rect.x = left
+       self.rect.y = top
+
+
 def display_lives (screen, lives):
     font = pygame.font.Font(None, 36)
-    text = font.render(f'Lives: {lives}', 1, (255, 255, 255))
+    text = font.render(f'Lives: {lives}', 1, TEXT_COLOR)
     textpos = text.get_rect()
     textpos.centerx = screen.get_rect().centerx
+    textpos.y = 10
     screen.blit(text, textpos)
 
 
 pygame.init()
 
-SCREEN_WIDTH = 500
-SCREEN_HEIGHT = 500
 
 lives = 10
+top_wall = Wall(SCREEN_WIDTH, WALL_WIDTH, 0, 40)
 
 # Set up the drawing window
 screen = pygame.display.set_mode([SCREEN_WIDTH, SCREEN_HEIGHT])
 
 # Run until the user asks to quit
 running = True
-
-BACKGROUND_COLOR = (0, 0, 0)
-
-display_items = []
 
 clock = pygame.time.Clock()
 
@@ -43,10 +63,7 @@ while running:
     background = background.convert()
     background.fill(BACKGROUND_COLOR)
 
-    # Draw a solid blue circle in the center
-    for i in display_items:
-        i.draw(background)
-
+    walls.draw(background)
     display_lives(background, lives)
 
     screen.blit(background, (0, 0))
